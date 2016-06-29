886004a2d47dcaa68d688472de2f53ba33d5e27f
umanoda <hhenpitu@gmail.com>
2016-06-29 21:47:15 +0900

    refactored test code

--- sokudoku_test.go
+++ sokudoku_test.go
@@ -2,30 +2,34 @@ package sokudoku
 
 import "testing"
 
+type testCase struct {
+	sample string
+	expect []string
+}
+
 func TestBuildPhrase(t *testing.T) {
 	p, _ := phraseInit()
 
-	got, _ := p.Parse("ぼくはくま。フワフワした赤い大きな熊。「やあ、こんにちは。」「やあ」と挨拶")
-
-	expects := []string{
-		"ぼくは",
-		"くま。",
-		"フワフワした",
-		"赤い",
-		"大きな",
-		"熊。",
-		"「やあ、",
-		"こんにちは。」",
-		"「やあ」と",
-		"挨拶",
-	}
-	for i, expect := range expects {
-		if expect != got[i] {
-			t.Error(
-				"\n   got:", got[i],
-				"\nexpect:", expect,
-			)
-			break
+	for _, test_case := range []testCase{
+		{
+			"ぼくはくま。フワフワした赤い大きな熊。「やあ、こんにちは。」「やあ」と挨拶",
+			[]string{"ぼくは", "くま。", "フワフワした", "赤い", "大きな", "熊。", "「やあ、", "こんにちは。」", "「やあ」と", "挨拶"},
+		},
+		{
+			"荒波に負けない心と、切なさと、優しさと。",
+			[]string{"荒波に", "負けない", "心と、", "切なさと、", "優しさと。"},
+		},
+	} {
+		got, _ := p.Parse(test_case.sample)
+		for i, expect := range test_case.expect {
+			if expect != got[i] {
+				t.Error(
+					"\n   got:", got[i],
+					"\nexpect:", expect,
+				)
+				break
+			}
 		}
 	}
+
 }
